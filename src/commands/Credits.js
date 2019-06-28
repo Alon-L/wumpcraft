82823be90697deb776fed7092846b60371c2346e
DayColor <2005alonlivne@gmail.com>
2019-06-28 20:28:35 +0300

    Added invite and credits commands.

---
+++ src/commands/Credits.js
@@ -0,0 +1,24 @@
+const Command = require('../types/Command');
+
+class Credits extends Command {
+  constructor() {
+    super();
+  }
+
+  run(client, msg) {
+    msg.channel.send('© Copyright 2019\n**</>** with :hearts: by DayColor#0001');
+  }
+}
+
+module.exports = {
+  run: Credits,
+  conf: {
+    aliases: []
+  },
+  help: {
+    name: 'credits',
+    description: 'Shows the credits for this bot.',
+    usage: 'credits',
+    priority: 5
+  }
+};
---
+++ src/commands/Invite.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const { grass } = require('../configs/blocks');
+const { embed: { colors: { main }, footer } } = require('../configs/default');
+const Command = require('../types/Command');
+
+class Invite extends Command {
+  constructor() {
+    super();
+  }
+
+  run(client, msg) {
+    const embedBuild = new Discord.RichEmbed()
+      .setTitle('Invite the bot to your server!')
+      .setColor(main)
+      .setTimestamp()
+      .setFooter(footer)
+      .setDescription(`${client.emojis.get(grass.id)} Click [here](https://discordapp.com/oauth2/authorize?client_id=592667565898334208&scope=bot&permissions=268774480) to add **WumpCraft** to your server!`)
+      .setImage('https://raw.githubusercontent.com/DayColor/wumpcraft/master/assets/header.png?token=AJBO3S5JYVPP4K4E2AW25C25D5ENU');
+    msg.channel.send({ embed: embedBuild });
+  }
+}
+
+module.exports = {
+  run: Invite,
+  conf: {
+    aliases: []
+  },
+  help: {
+    name: 'invite',
+    description: 'Sends the invitation link for the bot.',
+    usage: 'invite',
+    priority: 4
+  }
+};
