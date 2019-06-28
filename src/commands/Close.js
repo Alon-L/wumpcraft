ed8b9feb941049bc7ba8e6ab7d161efcf87fe7d0
DayColor <2005alonlivne@gmail.com>
2019-06-29 02:48:21 +0300

    Added a close command to close a member's game view.

---
+++ src/commands/Close.js
@@ -0,0 +1,29 @@
+const Command = require('../types/Command');
+const { getData } = require('../data');
+const { close } = require('../game/close');
+
+class Credits extends Command {
+  constructor() {
+    super();
+  }
+
+  run(client, msg) {
+    const d = getData(msg.author.id);
+    if (!d) return msg.channel.send('❌ You do not have an open game view.');
+    close(0, 'User closed the game.', d.world, msg.member);
+    msg.channel.send('🎮 You have successfully closed your game view.');
+  }
+}
+
+module.exports = {
+  run: Credits,
+  conf: {
+    aliases: []
+  },
+  help: {
+    name: 'close',
+    description: 'Close your open game view.',
+    usage: 'close',
+    priority: 5
+  }
+};
